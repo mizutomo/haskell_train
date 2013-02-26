57fbfb4e97f8ba422fe06541f7efe428e1b24baf
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2013-02-27 02:25:08 +0900

    Initial Commit

---
+++ baby.hs
@@ -0,0 +1,30 @@
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <-st, c `elem` ['A'..'Z']]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+  | otherwise     = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        skinny = 18.5
+        normal = 25.0
+        fat = 30.0
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ "." ++ [l] ++ "."
+  where (f:_) = firstname
+        (l:_) = lastname
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in sideArea + 2 * topArea
