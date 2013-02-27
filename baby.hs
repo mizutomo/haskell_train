42bb24adea7c875880bcaa91588a18ac6cf6baf8
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2013-02-27 23:35:13 +0900

    5.4まで

--- baby.hs
+++ baby.hs
@@ -84,3 +84,40 @@ quicksort (x:xs) =
   let smallerOrEqual = [a | a <- xs, a <= x]
       larger = [a | a <- xs, a > x]
   in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+  let smallerOrEqual = filter (<= x) xs
+      larger = filter (> x) xs
+  in quicksort' smallerOrEqual ++ [x] ++ quicksort' larger
+
+-- 5. ‚ŠKŠÖ”
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
+
+largestDivisible :: Integer
+largestDivisible = head (filter p [100000, 99999..])
+  where p x = x `mod` 3829 == 0
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd  n = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15)
+                                (map chain [1..100]))
