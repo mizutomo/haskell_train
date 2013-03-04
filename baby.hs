f5cd0efc8dd66a01b0ccd5116309e8144e17621b
Tomokatsu Mizukusa <tomokatsu.mizukusa.sx@renesas.com>
2013-03-04 17:37:45 +0900

    Adding 7

--- baby.hs
+++ baby.hs
@@ -148,3 +148,20 @@ digitSum = sum . map digitToInt . show
 
 firstTo :: Int -> Maybe Int
 firstTo n = find (\x -> digitSum x == n) [1..]
+
+-- 7. Type
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+area :: Shape -> Float
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
+
+
